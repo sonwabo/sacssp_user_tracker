6fa6ccadf2d4d66d1ec2377324ca58de727d25e0
Sonwabo Singatha <sonwabo@xelo-technologies.co.za>
2022-05-25 19:57:00 +0200

    Fixed the sheets generation

--- src/main/java/com/technologies/xelo/util/SheetsGenerator.java
+++ src/main/java/com/technologies/xelo/util/SheetsGenerator.java
@@ -15,7 +15,9 @@ import org.springframework.stereotype.Component;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.technologies.xelo.service.PartyManagerImpl.getUserDetailsDTO;
 
@@ -38,10 +40,10 @@ public class SheetsGenerator {
 
     public boolean generateSheets(int page, int size) throws IOException {
 
-        List<UserDetailsDTO> users = partyEntityRepository.findAll( PageRequest.of(0 , 10))
+        List<UserDetailsDTO> users = this.partyEntityRepository.findAll().stream() //partyEntityRepository.findAll( PageRequest.of(0 , 10))
                 .map(partyEntity -> {
                     return getUserDetailsDTO(partyEntity);
-                }).toList();
+                }).collect(Collectors.toList());
 
         if(!users.isEmpty()){
             HSSFWorkbook workbook = new HSSFWorkbook();
@@ -70,8 +72,20 @@ public class SheetsGenerator {
             rowhead.createCell(3).setCellValue(user.getName3());
             rowhead.createCell(4).setCellValue(user.getSurname());
             rowhead.createCell(5).setCellValue(user.getMaidensurname());
-            rowhead.createCell(6).setCellValue(user.getDisabilitystatus());
-            rowhead.createCell(7).setCellValue(user.getEthnicalstatus());
+
+            List<String> newList = Arrays.asList( user.getEthnicalstatus().split(",") ).stream()
+                    .distinct()
+                    .collect(Collectors.toList());
+            newList.removeAll(Arrays.asList("", null));
+
+            if(newList.size() > 1){
+                rowhead.createCell(6).setCellValue(newList.get(1));
+                rowhead.createCell(7).setCellValue(newList.get(0));
+            }else{
+                rowhead.createCell(6).setCellValue("No");
+                rowhead.createCell(7).setCellValue(newList.get(0));
+            }
+
             rowhead.createCell(8).setCellValue(user.getGender());
             rowhead.createCell(9).setCellValue(user.getLanguage());
             rowhead.createCell(10).setCellValue(user.getMaritalstatus());
@@ -81,26 +95,31 @@ public class SheetsGenerator {
             rowhead.createCell(14).setCellValue(user.getCitizenship());
             rowhead.createCell(15).setCellValue(user.getBirthdate());
             rowhead.createCell(16).setCellValue(user.getIdnumber());
+
+
             rowhead.createCell(17).setCellValue(user.getHouseorcomplexnameandnumber());
             rowhead.createCell(18).setCellValue(user.getStreetnumberandname());
             rowhead.createCell(19).setCellValue(user.getSuburbname());
             rowhead.createCell(20).setCellValue(user.getPostalcode());
             rowhead.createCell(21).setCellValue(user.getTownname());
             rowhead.createCell(22).setCellValue(user.getProvincename());
-            rowhead.createCell(23).setCellValue(user.getInstitutionname());
-            rowhead.createCell(24).setCellValue(user.getInstitutiontel());
-            rowhead.createCell(25).setCellValue(user.getUniversityobtained());
-            rowhead.createCell(26).setCellValue(user.getYearcompleted());
-            rowhead.createCell(27).setCellValue(user.getDurationofcourse());
+            rowhead.createCell(23).setCellValue(user.getDisabilitystatus());
 
-            rowhead.createCell(28).setCellValue(user.getQualificationlevel());
-            rowhead.createCell(29).setCellValue(user.getQualificationname());
-            rowhead.createCell(30).setCellValue(user.getEmployername());
-            rowhead.createCell(31).setCellValue(user.getJobtitle());
-            rowhead.createCell(32).setCellValue(user.getSectorofemployment());
-            rowhead.createCell(33).setCellValue(user.getEmpcellphone());
+
+            rowhead.createCell(24).setCellValue(user.getInstitutionname());
+            rowhead.createCell(25).setCellValue(user.getInstitutiontel());
+            rowhead.createCell(26).setCellValue(user.getUniversityobtained());
+            rowhead.createCell(27).setCellValue(user.getYearcompleted());
+            rowhead.createCell(28).setCellValue(user.getDurationofcourse());
+
+            rowhead.createCell(29).setCellValue(user.getQualificationlevel());
+            rowhead.createCell(30).setCellValue(user.getQualificationname());
+            rowhead.createCell(31).setCellValue(user.getEmployername());
+            rowhead.createCell(32).setCellValue(user.getJobtitle());
+            rowhead.createCell(33).setCellValue(user.getSectorofemployment());
             rowhead.createCell(34).setCellValue(user.getEmpcellphone());
-            rowhead.createCell(35).setCellValue(user.getEmail());
+            rowhead.createCell(35).setCellValue(user.getEmpcellphone());
+            rowhead.createCell(36).setCellValue(user.getEmail());
 
             counter = counter + 1;
         }
@@ -121,30 +140,34 @@ public class SheetsGenerator {
         rowhead.createCell(9).setCellValue("Language");
         rowhead.createCell(10).setCellValue("Marital Status");
         rowhead.createCell(11).setCellValue("Email");
-        rowhead.createCell(12).setCellValue("Work Number");
+        rowhead.createCell(12).setCellValue("Personal Telephone Number");
         rowhead.createCell(13).setCellValue("Cellphone");
         rowhead.createCell(14).setCellValue("Citezenship");
         rowhead.createCell(15).setCellValue("Birthdate");
         rowhead.createCell(16).setCellValue("Identification Number");
+
         rowhead.createCell(17).setCellValue("House/Complex Name and Number");
         rowhead.createCell(18).setCellValue("Street Name and Number");
         rowhead.createCell(19).setCellValue("Suburb Name");
         rowhead.createCell(20).setCellValue("Postal Code");
         rowhead.createCell(21).setCellValue("Town Name");
         rowhead.createCell(22).setCellValue("Province");
-        rowhead.createCell(23).setCellValue("Institution Name");
-        rowhead.createCell(24).setCellValue("Institution Tel");
-        rowhead.createCell(25).setCellValue("University Obtained");
-        rowhead.createCell(26).setCellValue("Year Completed");
-        rowhead.createCell(27).setCellValue("Duration of Course");
+        rowhead.createCell(23).setCellValue("District");
 
-        rowhead.createCell(28).setCellValue("Qualification Level");
-        rowhead.createCell(29).setCellValue("Qualification Name");
-        rowhead.createCell(30).setCellValue("Employer Name");
-        rowhead.createCell(31).setCellValue("Job Title");
-        rowhead.createCell(32).setCellValue("Sector of Employment");
-        rowhead.createCell(33).setCellValue("Employer Cellphone");
-        rowhead.createCell(34).setCellValue("Employer Work Number");
-        rowhead.createCell(35).setCellValue("Employer Email");
+
+        rowhead.createCell(24).setCellValue("Institution Name");
+        rowhead.createCell(25).setCellValue("Institution Tel");
+        rowhead.createCell(26).setCellValue("University Obtained");
+        rowhead.createCell(27).setCellValue("Year Completed");
+        rowhead.createCell(28).setCellValue("Duration of Course");
+
+        rowhead.createCell(29).setCellValue("Qualification Level");
+        rowhead.createCell(30).setCellValue("Qualification Name");
+        rowhead.createCell(31).setCellValue("Employer Name");
+        rowhead.createCell(32).setCellValue("Job Title");
+        rowhead.createCell(33).setCellValue("Sector of Employment");
+        rowhead.createCell(34).setCellValue("Employer Cellphone");
+        rowhead.createCell(35).setCellValue("Employer Work Number");
+        rowhead.createCell(36).setCellValue("Employer Email");
     }
 }
